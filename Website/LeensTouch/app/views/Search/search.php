b3da7234d1e82e445ae1b89b665b1c9abc070faa
Astour Krikor <72988512+KrikorAstour@users.noreply.github.com>
2022-04-24 16:35:11 -0700

    Add files via upload

---
+++ Website/LeensTouch/app/views/Search/search.php
@@ -0,0 +1,99 @@
+<?php require APPROOT . '/views/includes/header.php';?>
+
+<style>
+ body{
+  
+ }
+.product-card{
+  height: 450px;
+  width: 300px;
+  margin: 10px 60px;
+  display: none;
+  animation: blur .8s ease-out ;
+  
+}
+.product-container{
+  display: flex;
+    flex-direction: row;
+    flex-flow: row wrap;
+    flex-wrap: wrap;
+    justify-content: center;
+    width: 100%;
+    overflow: hidden;
+    overflow-x: hidden;
+    overflow-y: hidden; /* Hide vertical scrollbar */
+
+}
+
+.product-thumb {
+  height: 300px;
+  width: 300px;
+  margin-bottom: 10px;
+  border:1px solid black;
+  
+}
+
+
+
+.noContent {
+  pointer-events: none;
+  display: none!important;
+}
+
+
+.product-info{
+  border-style: solid;
+  border-width: 1px;
+
+  background-color:#e4c5bd;
+}
+</style>
+
+
+<div class="container-fluid">
+<h1>Catalog</h1>
+<div class="product-container">
+    <?php
+            foreach($data['products'] as $product){
+                 echo '
+                    <div class="product-card">
+                     <div class=" product-image">
+                         <form method="get" action="/LeensTouch/Product">
+                             <input type="hidden" name="product_id" value="'.$product->upc.'">
+                              <input type="image" id="image"
+                               src="'.$product->image.'" class="product-thumb" alt="" > 
+                        </form>
+                     </div>
+                    <div class=" product-info">
+                       <h5 class="product-brand">Name: '.$product->product_name.'</h2>
+                            <div class="price">Price: '.(number_format($product->price, 2, ',', ' ')).'$</div>
+                            '.(($product->quantity<'1')?'<div class="availability">Sold Out</div><div>&nbsp</div>':
+                                                        '<div class="availability">Available</div> 
+                                                         <a class="card-btn" href="http://">Add to cart</a>'
+                             ).'
+                        </div>
+                    </div>  
+                ';             
+            }
+    ?>   
+</div>
+
+<button id = "loadMore" type="button" class="btn btn-primary btn-lg rounded-3 mx-auto mb-5" style="display: block; background-color:#e4c5bd; border: 1px solid #000000; color: #000000; ">
+        Load More</button>
+        
+   <script src ="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"> </script>
+<script src ="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"> </script>
+<script>
+  $(document).ready(function () {
+  $(".product-card").slice(0, 4).show();
+  $("#loadMore").on("click", function(e){
+    e.preventDefault();
+    $(".product-card:hidden").slice(0, 4).slideDown();
+    if ($(".product-card:hidden").length == 0) {
+      $("#loadMore").text("No Content").addClass("noContent");
+    }
+  });
+  })
+</script>
+
+<?php require APPROOT . '/views/includes/footer.php'; ?>

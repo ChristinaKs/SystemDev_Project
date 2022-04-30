3b3b465bb275b4c4672114da2d895ae0377d40ab
Veaceslav Vlas <vlasslavic@gmail.com>
2022-04-30 15:45:53 -0400

    Image URL fix

--- Website/LeensTouch/app/views/Product/product.php
+++ Website/LeensTouch/app/views/Product/product.php
@@ -2,7 +2,7 @@
 
 
         <div class="product-image mt-0">
-            <img src=" <?php echo $data->image ?>" class=" float-start img-fluid" alt="" style="width:35%">  
+            <img src=" <?php echo URLROOT.'/public/img/'.$data->image ?>" class=" float-start img-fluid" alt="" style="width:35%">  
         </div>
         
         <div class="product-info d-grid ms-5 mt-0">

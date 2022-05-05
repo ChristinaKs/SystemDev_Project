a7ab84f3110521c8e2fa6b1d40310b679579ef1c
ChristinaKs <74264367+ChristinaKs@users.noreply.github.com>
2022-05-05 00:05:47 -0400

    Update createProduct.php
    fixed indents

--- Website/LeensTouch/app/views/AdminProducts/createProduct.php
+++ Website/LeensTouch/app/views/AdminProducts/createProduct.php
@@ -67,36 +67,36 @@
     </div>
         
         
-    <!-- <div class="product-info mt-0"> --> <div class="product-info mt-0">
+    <div class="product-info d-grid ms-5 mt-0">  <!-- <div class="product-info mt-0"> -->
       <div class="ms-5 mb-3 mt-5"><span class="h5 ">Name:</span> <br> 
           <div class="form-group" style="margin: auto; float: left;">
             <input name="product_name" type="text" class="form-control" id="product_name" placeholder="Name of the product" style="width: 400px;">
           </div> 
-        </div><br><br>
+        </div><br>
 
         <div class="ms-5 mb-3"><span class="h5 ">Price:</span> <br>
           <div class="form-group" style="margin: auto; float: left;">
             <input name="price" type="text" class="form-control" id="price" placeholder="Price of the product" style="width: 400px;">
           </div>
-        </div><br><br>
+        </div><br>
 
         <div class=" ms-5 mb-3 me-5"><span class="h5">Product Description:</span><br>
           <div class="form-group" style=" margin: auto; float: left;">
             <input name="description" type="text" class="form-control" id="description" placeholder="Description of the product" style="width: 400px;">
           </div>
-        </div> <br><br>
+        </div> <br>
 
         <div class="h5 ms-5"><span class="h5">Colors Available:</span><br>
           <div class="form-group" style="margin: auto; float: left;">
             <input name="colour" type="text" class="form-control" id="colour" placeholder="Product colour" style="width: 400px;">
           </div>
-        </div> <br><br>
+        </div> <br>
           
         <div class="ms-5 mb-3"><span class="h5 ">Quantity Available:</span> <br>
           <div class="form-group" style=" margin: auto; float: left;" style="width: 400px;">
             <input name="quantity" type="text" class="form-control" id="quantity" placeholder="Quantity available" style="width: 400px;">
           </div>
-        </div> <br><br>
+        </div> <br>
           
         <div class="form-group" style=" margin-left: 100px; float: left">
           <button type="submit" name='register' class="btn btn-primary" style="background-color: #e4c5bd; color: white;">Register</button>

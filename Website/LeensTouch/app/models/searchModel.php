2ca3a39c94dae9755a12ffde8630a8f69df52830
Krikor Astour <ararad.seller@gmail.com>
2022-05-10 05:41:36 -0700

    search updated

--- Website/LeensTouch/app/models/searchModel.php
+++ Website/LeensTouch/app/models/searchModel.php
@@ -3,22 +3,29 @@
         public function __construct(){
             $this->db = new Model;
         }
+        // public function getResultByName($product_name){
+        //     $this->db->query("SELECT * FROM products
+        //                         WHERE product_name LIKE '%:product_name%'");
+        //     $this->db->bind(':product_name', $product_name);
+        //     return $this->db->getResultSet();
+        // }
+
         public function getResultByName($product_name){
             $this->db->query("SELECT * FROM products
-                                WHERE product_name LIKE '%:product_name%'");
+                                WHERE product_name = :product_name");
             $this->db->bind(':product_name', $product_name);
             return $this->db->getResultSet();
         }
 
         public function sortByPriceHighest(){
             $this->db->query("SELECT * FROM products
-                                ORDER BY product_price");
+                                ORDER BY price DESC");
             return $this->db->getResultSet();
         }
 
         public function sortByPriceLowest(){
             $this->db->query("SELECT * FROM products
-                                ORDER BY product_price");
+                                ORDER BY price");
             return $this->db->getResultSet();
         }
 
